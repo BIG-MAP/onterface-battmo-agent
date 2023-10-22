78bd82bc96cc305e65a8eb54ffda4fa8cefbc097
SimonTaurus <simon.stier@gmx.de>
2023-10-22 17:38:47 +0000

    fix: absolute path, changed oct2py syntax

--- prefect/flows/performance_spec/battmo_prefect_flow.py
+++ prefect/flows/performance_spec/battmo_prefect_flow.py
@@ -59,22 +59,25 @@ class PerformanceSpecResponse(BaseModel):
 
 @flow
 def run_performance_spec(request: PerformanceSpecRequest):
-    octave.run('/home/jovyan/BattMo/startupBattMo.m')
+    octave.run('/root/flows/BattMo/startupBattMo.m')
     
     print(request.geometry.json())
     
-    battmo_input = f'/home/jovyan/BattMo/Examples/experiment/optimization_test/input_json/{str(request.uuid)}.json'
-    battmo_output = f'/home/jovyan/BattMo/Examples/experiment/optimization_test/output_json/{str(request.uuid)}.json' 
+    battmo_input = f'/root/flows/BattMo/Examples/{str(request.uuid)}.json'
+    battmo_output = f'/root/flows/BattMo/Examples/{str(request.uuid)}.json' 
     
-    f = open(battmo_input, "w")
-    f.write(request.geometry.json())
-    f.close()
+    with open(battmo_input, "w") as f:
+        f.write(request.geometry.json())
         
     # run the simulation
-    battmo_input = f'Examples/experiment/optimization_test/input_json/{str(request.uuid)}.json'
-    E, energyDensity, energy = octave.runJsonFunction({'ParameterData/BatteryCellParameters/LithiumIonBatteryCell/lithium_ion_battery_nmc_graphite.json', battmo_input}, battmo_output, nout=3)
+    battmo_input = f'Examples/{str(request.uuid)}.json'
+    #E, energyDensity, energy = octave.runJsonFunction({'ParameterData/BatteryCellParameters/LithiumIonBatteryCell/lithium_ion_battery_nmc_graphite.json', battmo_input}, battmo_output, nout=3)
+    E, energyDensity, energy = octave.feval('runJsonFunction', {'ParameterData/BatteryCellParameters/LithiumIonBatteryCell/lithium_ion_battery_nmc_graphite.json', battmo_input}, battmo_output, True, nout=3)
     response = PerformanceSpecResponse(
         uuid=request.uuid,
         result=PerformanceSpec(E = E[:,0][-1], energyDensity = energyDensity[:,0][-1], energy = energy[:,0][-1])
     )
     return response
+
+if __name__ == "__main__":
+     run_performance_spec(request=PerformanceSpecRequest())
